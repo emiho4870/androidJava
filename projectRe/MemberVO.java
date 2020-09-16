6ae9212e00fc6666f62cab8672cf0301ffbe7358
emiho4870 <53415319+emiho4870@users.noreply.github.com>
2020-09-16 12:03:19 +0900

    Add files via upload

---
+++ projectRe/MemberVO.java
@@ -0,0 +1,51 @@
+package projectRe;
+
+public class MemberVO {
+	
+	private String member_id;
+	private String member_pw;
+	private String member_pwChk;
+	private String member_name;
+	private String member_tel_no;
+	private String member_address;
+	
+	
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getMember_pw() {
+		return member_pw;
+	}
+	public void setMember_pw(String member_pw) {
+		this.member_pw = member_pw;
+	}
+	public String getMember_pwChk() {
+		return member_pwChk;
+	}
+	public void setMember_pwChk(String member_pwChk) {
+		this.member_pwChk = member_pwChk;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getMember_tel_no() {
+		return member_tel_no;
+	}
+	public void setMember_tel_no(String member_tel_no) {
+		this.member_tel_no = member_tel_no;
+	}
+	public String getMember_address() {
+		return member_address;
+	}
+	public void setMember_address(String member_address) {
+		this.member_address = member_address;
+	}
+	
+	
+}
